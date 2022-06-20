1bc4958857c5a15b3da1eff5fe8e84b782141b3a
Emily Sturman <sturman@fb.com>
2022-06-20 11:21:32 -0700

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import * as React from "react"
-// IMPORT ANY NEEDED COMPONENTS HERE
+import Header from "./components/Header/Header";
+import Instructions from "./components/Instructions/Instructions";
+import Chip from "./components/Chip/Chip";
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel";
 import { createDataSet } from "./data/dataset"
 import "./App.css"
 
@@ -20,43 +23,96 @@ export const appInfo = {
 // or this!
 const { data, categories, restaurants } = createDataSet()
 
+const CategoriesColumn = ({ categories, state, setState }) => (
+  <div className="CategoriesColumn col">
+    <div className="categories options">
+      <h2 className="title">Categories</h2>
+      {categories.map((category) => (
+        <Chip
+          key={category}
+          label={category}
+          onClick={() => setState(category)}
+          onClose={() => setState(null)}
+          isActive={category === state}
+        />
+      ))}
+    </div>
+  </div>
+)
+
+const RestaurantsRow = ({ restaurants, state, setState }) => (
+  <div className="RestaurantsRow">
+    <h2 className="title">Restaurants</h2>
+    <div className="restaurants options">
+      {restaurants.map((restaurant) => (
+        <Chip
+          key={restaurant}
+          label={restaurant}
+          onClick={() => setState(restaurant)}
+          onClose={() => setState(null)}
+          isActive={restaurant === state}
+        />
+      ))}
+    </div>
+  </div>
+);
+
+const MenuDisplay = ({ menuItems, state, setState }) => (
+  <div className="MenuDisplay display">
+    <div className="MenuItemButtons menu-items">
+      <h2 className="title">Menu Items</h2>
+      {menuItems.map((item) => (
+        <Chip
+          key={item.item_name}
+          label={item.item_name}
+          onClick={() => setState(item)}
+          onClose={() => setState(null)}
+          isActive={state && item.item_name === state.item_name}
+        />
+      ))}
+    </div>
+    <div className="NutritionFacts nutrition-facts">
+      {state && <NutritionalLabel item={state} />}
+    </div>
+  </div>
+);
+
+const DataSource = ({ dataSource }) => (
+  <div className="data-sources">
+    <p>{dataSource}</p>
+  </div>
+)
+
 export function App() {
+  const [selectedCategory, setSelectedCategory] = React.useState();
+  const [selectedRestaurant, setSelectedRestaurant] = React.useState();
+  const [selectedMenuItem, setSelectedMenuItem] = React.useState();
+
+  React.useEffect(() => {
+    setSelectedMenuItem(null);
+  }, [selectedCategory, selectedRestaurant]);
+
+  const currentMenuItems = data.filter(({ food_category, restaurant }) => (
+    food_category === selectedCategory && restaurant === selectedRestaurant
+  ));
+
+  const instruction = (
+    !selectedCategory && !selectedRestaurant && !selectedMenuItem ? appInfo.instructions.start
+      : selectedCategory && !selectedRestaurant && !selectedMenuItem ? appInfo.instructions.onlyCategory
+      : selectedRestaurant && !selectedCategory && !selectedMenuItem ? appInfo.instructions.onlyRestaurant
+      : selectedCategory && selectedRestaurant && !selectedMenuItem ? appInfo.instructions.noSelectedItem
+      : appInfo.instructions.allSelected
+  );
+
   return (
     <main className="App">
-      {/* CATEGORIES COLUMN */}
-      <div className="CategoriesColumn col">
-        <div className="categories options">
-          <h2 className="title">Categories</h2>
-          {/* YOUR CODE HERE */}
-        </div>
-      </div>
-
-      {/* MAIN COLUMN */}
+      <CategoriesColumn categories={categories} state={selectedCategory} setState={setSelectedCategory} />
       <div className="container">
-        {/* HEADER GOES HERE */}
-
-        {/* RESTAURANTS ROW */}
-        <div className="RestaurantsRow">
-          <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
-        </div>
-
-        {/* INSTRUCTIONS GO HERE */}
-
-        {/* MENU DISPLAY */}
-        <div className="MenuDisplay display">
-          <div className="MenuItemButtons menu-items">
-            <h2 className="title">Menu Items</h2>
-            {/* YOUR CODE HERE */}
-          </div>
-
-          {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
-        </div>
-
-        <div className="data-sources">
-          <p>{appInfo.dataSource}</p>
-        </div>
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description} />
+        <RestaurantsRow restaurants={restaurants} state={selectedRestaurant} setState={setSelectedRestaurant} />
+        <Instructions instructions={instruction} />
+        <MenuDisplay menuItems={currentMenuItems} state={selectedMenuItem} setState={setSelectedMenuItem} />
+        <DataSource dataSource={appInfo.dataSource} />
       </div>
     </main>
   )
--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,11 +1,15 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ label = "", isActive = false }) {
+export function Chip({ label = "", isActive = false, onClick, onClose }) {
+  const buttonClassName = isActive ? "chip active" : "chip";
   return (
-    <button className="chip">
-      <p className="label"></p>
-      <span className="close" role="button">{`X`}</span>
+    <button className={buttonClassName} onClick={onClick}>
+      <p className="label">{label}</p>
+      <span className="close" role="button" onClick={(e) => {
+        onClose();
+        e.stopPropagation();
+      }}>{`X`}</span>
     </button>
   )
 }
